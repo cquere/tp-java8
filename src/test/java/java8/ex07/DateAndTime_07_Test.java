c82740c16e2f41dededbb17bff0540897a8c9271
cyril.quere <cyril.quere@gmail.com>
2018-03-29 16:56:37 +0200

    exs DateAndTime

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -3,8 +3,10 @@ package java8.ex07;
 import org.junit.Test;
 
 import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -15,23 +17,25 @@ import static org.junit.Assert.assertThat;
  */
 public class DateAndTime_07_Test {
 
-    @Test
-    public void test_date_to_localdate() throws Exception {
+	@Test
+	public void test_date_to_localdate() throws Exception {
 
-        // TODO Créer une date Java 1 (12/02/2017)
-        Date date = null;
+		// TODO Créer une date Java 1 (12/02/2017)
+		Date date = new Date(117, 01, 12);
 
-        // TODO transformer la date en Instant
+		// TODO transformer la date en Instant
+		Instant i = date.toInstant();
+		// TODO transformer la date en LocalDate
 
-        // TODO transformer la date en LocalDate
+		LocalDate result = i.atZone(ZoneId.systemDefault())
+			      .toLocalDate();
 
-        LocalDate result = null;
+		// TODO valoriser les différentes variables afin de rendre le test
+		// passant
 
-        // TODO valoriser les différentes variables afin de rendre le test passant
+		assertThat(result.getYear(), is(2017));
+		assertThat(result.getMonth(), is(Month.FEBRUARY));
+		assertThat(result.getDayOfMonth(), is(12));
 
-        assertThat(result.getYear(), is(2017));
-        assertThat(result.getMonth(), is(Month.FEBRUARY));
-        assertThat(result.getDayOfMonth(), is(12));
-
-    }
+	}
 }
